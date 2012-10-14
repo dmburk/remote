5d2ecc7f1e8257a4bf9d3f24df2a36e586f461cc
Michael Burk <michael.burk@gmail.com>
2012-10-14 11:31:08 -0400

    Renamed program files.

--- servx.rb
+++ servx.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/ruby
 # Serv-X
 # This script will display a list of servers and connect via SSH to the selected server.
-require "servx_controller.rb"
-require "servx_model.rb"
-require "servx_view.rb"
+require "connection.rb"
+require "servers.rb"
+require "views.rb"
 
 @connection = Connection.new
 @connection.list
